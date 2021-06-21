0bfe433ce74eab9255be8ad9fa47146ae0447eeb
valentin bourcet <valentin.bourcet@gmail.com>
2021-06-21 17:58:51 +0200

    refonte connection et inscription

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class InscriptionController extends AbstractController
 {
@@ -16,5 +19,15 @@ class InscriptionController extends AbstractController
         return $this->render('inscription/index.html.twig', [
             'controller_name' => 'InscriptionController',
         ]);
+    }
+    /**
+     * @Route("/inscription/create", name="app_inscription_create", methods={"POST"})
+     */
+    public function create(Request $request,EntityManagerInterface $em):Response
+    {
+        if($request->isMethod('POST')){
+            $data=$request->request->all();       
+        }
+        return $this->redirect('/inscription');
     }
 }
