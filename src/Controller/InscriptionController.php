7c50c18eb683ed6595156beb8a893c8a6c6ab0fd
valentin bourcet <valentin.bourcet@gmail.com>
2021-06-23 15:28:14 +0200

    framework user

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -28,13 +28,17 @@ class InscriptionController extends AbstractController
     {
         $User=new User;
         $repo=$em->getRepository(User::class);
-        //$session=new Session;
-        //dd($session->get('role'));
-        $User=$repo->findAll();
-        return $this->render('inscription/index.html.twig', [
+        $session=new Session;
+        if($session->get('role')=="admin"){
+            $User=$repo->findAll();
+            return $this->render('inscription/index.html.twig', [
             'controller_name' => 'InscriptionController',
             'users'=> $User,
         ]);
+        }else{
+            return $this->redirectToRoute('connection');
+        }
+        
     }
     /**
      * @Route("/inscription/create", name="app_inscription_create",methods={"GET","POST"})
@@ -62,10 +66,49 @@ class InscriptionController extends AbstractController
             $User->setRole($data['role']);
             $em->persist($User);
             $em->flush();
-            return $this->redirectToRoute('inscription');    
+            return $this->redirectToRoute('connection');    
         }
         return $this->render('inscription/create.html.twig',
         ['form'=>$form->createView(),
             ]);
+    }
+    /**
+     * @Route("inscription/{id}/edit", name="app_inscription_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request,EntityManagerInterface $em,User $user): Response
+    {
+        $form = $this->createFormBuilder()
+        ->add('name',TextType::class,['data'=>$user->getUsername()])
+        ->add('email',EmailType::class,['data'=>$user->getEmail()])
+        ->add('role',ChoiceType::class,[
+            'choices'=>['admin'=>'admin','Responsable Annee'=>'Responsable Annee','Reponsable Module'=>'Responsable Module']
+        ],['data'=>$user->getRole()])
+            ->getForm()
+            ->add('submit',SubmitType::class, ['label'=>"modifier"])
+        ;
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $data=$form->getData();
+            $user->setUsername($data['name']);
+            $user->setEmail($data['email']);
+            $user->setRole($data['role']);
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('inscription');
+        }
+        return $this->render('inscription/edit.html.twig',
+            ['form'=>$form->createView(),
+                'user'=>$user
+                ]);
+    }
+    /**
+     * @Route("inscription/{id}/delete", name="app_inscription_delete", methods={"GET","POST"})
+     */
+    public function delete(Request $request,EntityManagerInterface $em,User $user): Response
+    {
+        $em->remove($user);
+        $em->flush();
+        return $this->redirectToRoute('inscription');
     }
 }
