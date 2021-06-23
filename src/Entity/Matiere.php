1b7667d82685bc05d105f1b0db2685ee8b2e4ef8
valentin bourcet <valentin.bourcet@gmail.com>
2021-06-23 17:30:00 +0200

    entite matiere

---
+++ src/Entity/Matiere.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MatiereRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MatiereRepository::class)
+ */
+class Matiere
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255,unique=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=3)
+     */
+    private $annee;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $departement;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $semestre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $module;
+
+    /**
+     * @ORM\Column(type="text",  nullable=true)
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $introduction;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $prerequis;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?string
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(string $annee): self
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    public function getDepartement(): ?string
+    {
+        return $this->departement;
+    }
+
+    public function setDepartement(string $departement): self
+    {
+        $this->departement = $departement;
+
+        return $this;
+    }
+
+    public function getSemestre(): ?int
+    {
+        return $this->semestre;
+    }
+
+    public function setSemestre(int $semestre): self
+    {
+        $this->semestre = $semestre;
+
+        return $this;
+    }
+
+    public function getModule(): ?string
+    {
+        return $this->module;
+    }
+
+    public function setModule(string $module): self
+    {
+        $this->module = $module;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getIntroduction(): ?string
+    {
+        return $this->introduction;
+    }
+
+    public function setIntroduction(?string $introduction): self
+    {
+        $this->introduction = $introduction;
+
+        return $this;
+    }
+
+    public function getPrerequis(): ?string
+    {
+        return $this->prerequis;
+    }
+
+    public function setPrerequis(?string $prerequis): self
+    {
+        $this->prerequis = $prerequis;
+
+        return $this;
+    }
+}
