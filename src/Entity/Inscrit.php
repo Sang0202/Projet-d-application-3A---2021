f7d94b4256b6b2d06b9af5e3d7eea7c400a3b65b
valentin bourcet <valentin.bourcet@gmail.com>
2021-06-24 19:06:10 +0200

    create entity Inscrit

--- src/Entity/Inscrit.php
+++ src/Entity/Inscrit.php
@@ -20,21 +20,123 @@ class Inscrit
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $name;
+    private $Username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Role;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $departement;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $annee;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $IdEnseignant;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getUsername(): ?string
     {
-        return $this->name;
+        return $this->Username;
     }
 
-    public function setName(string $name): self
+    public function setUsername(string $Username): self
     {
-        $this->name = $name;
+        $this->Username = $Username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $Password): self
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->Role;
+    }
+
+    public function setRole(string $Role): self
+    {
+        $this->Role = $Role;
+
+        return $this;
+    }
+
+    public function getDepartement(): ?string
+    {
+        return $this->departement;
+    }
+
+    public function setDepartement(?string $departement): self
+    {
+        $this->departement = $departement;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?string
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(?string $annee): self
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    public function getIdEnseignant(): ?int
+    {
+        return $this->IdEnseignant;
+    }
+
+    public function setIdEnseignant(int $IdEnseignant): self
+    {
+        $this->IdEnseignant = $IdEnseignant;
 
         return $this;
     }
